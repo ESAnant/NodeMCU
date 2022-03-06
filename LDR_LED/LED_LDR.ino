89283a9c4ba12cde97ff3a91786eff9b4b70f78c
Edidi Sai Anant <89198001+ESAnant@users.noreply.github.com>
2022-03-06 23:24:21 +0530

    Update LED_LDR.ino

--- LDR_LED/LED_LDR.ino
+++ LDR_LED/LED_LDR.ino
@@ -1,15 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-pinMode(A0,INPUT);
-pinMode(16,OUTPUT);
+pinMode(A0,INPUT);//Connect the signal pin of LDR to A0
+pinMode(16,OUTPUT);//Connect the LED pin to 16
 }
 
 void loop() {
-  int data=analogRead(A0);
-  if(data==1024)
-  digitalWrite(16,LOW);
+  int data=analogRead(A0);//Get the readings from A0
+  if(data==1024)//When the signal reaches 1024(dark)
+  digitalWrite(16,HIGH);//Turn ON LED
   else
-  digitalWrite(16,HIGH);
+  digitalWrite(16,LOW);//Turn OFF LED
   // put your main code here, to run repeatedly:
-
 }
